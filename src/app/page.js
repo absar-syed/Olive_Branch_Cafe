b776521713a1037f20b3d92df9f60336ee32aece
Absar Syed <absar.h.syed@gmail.com>
2025-06-03 18:46:13 -0400

    page in progress

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,8 @@
 export default function Home() {
-    
+    return(
+        <div className="d-flex justify-content-center"> 
+            <h1 className="page-h1 ">A new Aesthetic cafe now in Oshawa</h1>
+            <p className="page-p">Where every sip tastes like home</p>  
+        </div>
+    );
 }
