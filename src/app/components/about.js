015bd6d90849c5352fda6119df0980820c987c86
Absar Syed <absar.h.syed@gmail.com>
2025-06-11 19:42:05 -0400

    deleted pictures talking up space, added card to about

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -1,15 +1,31 @@
+import Image from "next/image";
 
 export default function About() {
     return(
-        <section className="page-section bg-primary" id="about">
+        <section className="page-section" id="about">
             <div className="container px-4 px-lg-5">
                 <div className="row gx-4 gx-lg-5 justify-content-center">
-                    <div className="col-lg-8 text-center">
+                    {/* <div className="col-lg-8 text-center">
                         <h2 className="text-white mt-0">We've got what you need!</h2>
-                        <hr className="divider divider-light" />
+                        <hr className="divider" />
                         <p className="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
-                        <a className="btn btn-light btn-xl" href="#services">Get Started!</a>
+                    </div> */}
+
+                    {/* CARD  */}
+                    <div className="card mb-3" >
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                            <Image src="/olive.webp" width={400} height={800} className="img-fluid rounded-start" alt="..."/>
+                            </div>
+                            <div className="col-md-8">
+                            <div className="card-body">
+                                <h2 className="card-title">Card title</h2>
+                                
+                            </div>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </section>
--- src/app/components/header.js
+++ src/app/components/header.js
@@ -38,22 +38,22 @@ export default function Header() {
     //   </div>
     // </nav>
 
-            <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-            <div className="container px-4 px-lg-5">
-                <a className="navbar-brand" href="#page-top">
-                    <Image src="/favicon.ico" alt='olive branch logo' width={60} height={60}/>
-                    <Image src="/OliveBranch.png" alt='olive branch cafe logo' width={130} height={60}/>
-                </a>
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ms-auto my-2 my-lg-0">
-                        <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#services">Services</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
-                    </ul>
-                </div>
+    <nav className="navbar navbar-expand-lg fixed-top py-3" id="mainNav">
+        <div className="container px-4 px-lg-5">
+            <a className="navbar-brand" href="#page-top">
+                <Image src="/favicon.ico" alt='olive branch logo' width={60} height={60}/>
+                <Image src="/OliveBranch.png" alt='olive branch cafe logo' width={130} height={60}/>
+            </a>
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ms-auto my-2 my-lg-0">
+                    <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
+                    <li className="nav-item"><a className="nav-link" href="#services">Services</a></li>
+                    <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
+                    <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
   );
 }
